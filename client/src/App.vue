99c1414c53a32465bceb816411da7d7ded61397d
gbasbu <gorkembasbu@gmail.com>
2021-01-30 20:50:33 +0300

    set template

---
+++ client/src/App.vue
@@ -0,0 +1,50 @@
+<template>
+  <nav>
+    <router-link to="/">
+      <img src="./assets/logo.svg" alt="Logo">
+    </router-link>
+    <ul>
+      <li>
+        <router-link to="/user"><i class="fas fa-user"></i></router-link>
+      </li>
+      <li>
+        <router-link to="/basket"><i class="fas fa-shopping-basket"></i></router-link>
+      </li>
+    </ul>
+  </nav>
+  <router-view/>
+</template>
+
+<style lang="scss" scoped>
+nav{
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: 0 20px;
+  @media (min-width:768px) {
+    padding: 0 40px;
+  }
+  @media (min-width:1024px) {
+    padding: 0 80px;
+  }
+  ul{
+    list-style: none;
+    display: flex;
+    li:nth-last-child(1){
+      margin-left: 20px;
+
+      @media (min-width:768px) {
+      margin-left: 30px;
+      }
+      @media (min-width:1024px) {
+        margin-left: 40px;
+      }
+    }
+    i{
+      font-size: 2rem;
+      color: black;
+    }
+  }
+}
+
+</style>
---
+++ client/src/router/index.js
@@ -0,0 +1,34 @@
+import { createRouter, createWebHistory } from 'vue-router'
+import Home from '../views/Home.vue'
+
+
+const routes = [
+  {
+    path: '/',
+    name: 'Home',
+    component: Home
+  },
+  {
+    path: '/user',
+    name: 'User',
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () => import(/* webpackChunkName: "about" */ '../views/User.vue')
+  },
+  {
+    path: '/basket',
+    name: 'Basket',
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () => import(/* webpackChunkName: "about" */ '../views/Basket.vue')
+  }
+]
+
+const router = createRouter({
+  history: createWebHistory(process.env.BASE_URL),
+  routes
+})
+
+export default router
