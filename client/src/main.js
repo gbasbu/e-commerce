30cb62679171bb71dec7bb7aad11eeadc3f8d5d3
gbasbu <gorkembasbu@gmail.com>
2021-01-31 18:42:11 +0300

    set user token

--- client/src/main.js
+++ client/src/main.js
@@ -2,8 +2,17 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import axios from 'axios'
 
 Vue.config.productionTip = false
+
+Vue.prototype.$http = axios;
+
+const token = localStorage.getItem('token');
+
+if(token) {
+  Vue.prototype.$http.defaults.headers.common['Authorization'] = token;
+}
 
 new Vue({
   router,
