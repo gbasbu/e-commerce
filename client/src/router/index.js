482b1c10d737c2f7518d92746aea6f06e82f7613
gbasbu <gorkembasbu@gmail.com>
2021-02-01 15:53:51 +0300

    added router guard

--- client/src/router/index.js
+++ client/src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Home from '../views/Home.vue'
+import store from '../store'
 
 Vue.use(VueRouter)
 
@@ -13,24 +14,53 @@ const routes = [
   {
     path: '/login',
     name: 'Login',
-    component: () => import(/* webpackChunkName: "about" */ '../views/Login.vue')
+    component: () => import(/* webpackChunkName: "about" */ '../views/Login.vue'),
+    meta:{
+      requiresGuest: true
+    }
   },
   {
     path: '/register',
     name: 'Register',
-    component: () => import(/* webpackChunkName: "about" */ '../views/Register.vue')
+    component: () => import(/* webpackChunkName: "about" */ '../views/Register.vue'),
+    meta:{
+      requiresGuest: true
+    }
   },
   {
     path: '/profile',
     name: 'Profile',
-    component: () => import(/* webpackChunkName: "about" */ '../views/Profile.vue')
+    component: () => import(/* webpackChunkName: "about" */ '../views/Profile.vue'),
+    meta:{
+      requiresAuth: true
+    }
   },
 ]
+
 
 const router = new VueRouter({
   mode: 'history',
   base: process.env.BASE_URL,
   routes
 })
+
+router.beforeEach((to, from, next) => {
+  if(to.matched.some(record => record.meta.requiresAuth)) {
+    if(!store.getters.isLoggedIn){
+      next('/login')
+    }else{
+      next()
+    }
+  }else if (to.matched.some(record => record.meta.requiresGuest)){
+    if(store.getters.isLoggedIn){
+      next('/home')
+    }else{
+      next()
+    }
+  }else{
+    next()
+  }
+})
+
 
 export default router
