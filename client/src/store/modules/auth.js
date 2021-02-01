c6dab0018f51b9ed9f16b0d9b53249aee73920cb
gbasbu <gorkembasbu@gmail.com>
2021-02-01 15:54:30 +0300

    added get action

--- client/src/store/modules/auth.js
+++ client/src/store/modules/auth.js
@@ -1,5 +1,5 @@
 import axios from 'axios'
-
+import router from '../../router'
 
 export const state = {
     token: localStorage.getItem('token') || '',
@@ -21,7 +21,25 @@ export const mutations = {
         state.token = token,
         state.user = user,
         state.status = 'success'
+    },
+    register_request(state){
+        state.status = 'loading'
+    },
+    register_success(state){
+        state.status = 'success'
+    },
+    logout(state){
+        state.status = ''
+        state.token = ''
+        state.user = ''
+    },
+    profile_request(state){
+        state.status = 'loading'
+    },
+    profile_response(state,user){
+        state.user = user
     }
+    
 };
 
 export const actions = {
@@ -39,6 +57,30 @@ export const actions = {
             commit('auth_success', token, user);
         }
         return res;
+    },
+    // Register Action
+    async register({ commit }, userData){
+        commit('register_request');
+        let res = await axios.post('http://localhost:5000/api/users/register', userData);
+        if(res.data.success !== undefined){
+            commit('register_success');
+        }
+        return res;
+    },
+    // Get Profile
+    async getProfile({ commit }) {
+        commit('profile_request')
+        let res = await axios.get('http://localhost:5000/api/users/profile')
+        commit('profile_response', res.data.user)
+        return res
+    },
+    // Logout Action
+    async logout({ commit }){
+        await localStorage.removeItem('token');
+        commit('logout');
+        delete axios.defaults.headers.common['Authorization'];
+        router.push('/login')
+        return
     }
 };
 
