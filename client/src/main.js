1657cf66b02f09f5e5efb38eccd795e790e6b126
gbasbu <gorkembasbu@gmail.com>
2021-02-01 15:53:30 +0300

    added comments

--- client/src/main.js
+++ client/src/main.js
@@ -5,11 +5,11 @@ import store from './store'
 import axios from 'axios'
 
 Vue.config.productionTip = false
-
+// ENG: Setting up default vue's http modules for api calls & TR: Http modülü axios olarak atandı
 Vue.prototype.$http = axios;
-
+// ENG: Load the token from the localStorage & TR: Token yüklendi
 const token = localStorage.getItem('token');
-
+// ENG: Is there is any token then we will simply append default axios authorization headers & TR: Token varsa doğrulama atandı
 if(token) {
   Vue.prototype.$http.defaults.headers.common['Authorization'] = token;
 }
