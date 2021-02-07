9a5e46a5d9d4cf70aa49e5af7c9af0551782f394
gbasbu <gorkembasbu@gmail.com>
2021-02-07 21:35:00 +0300

    added new module

--- client/src/store/index.js
+++ client/src/store/index.js
@@ -1,13 +1,15 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
-import * as auth from './modules/auth'
+import * as user from './modules/user'
+import * as  address from './modules/address'
 
 Vue.use(Vuex)
 
 export default new Vuex.Store({
   
   modules: {
-    auth
+    user,
+    address
   }
 
 })
