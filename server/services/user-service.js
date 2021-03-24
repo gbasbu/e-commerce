8d3c9fcce5d747cca907bcf9544e0188fa07d125
gbasbu <gorkembasbu@gmail.com>
2021-03-24 20:25:33 +0300

    added like-dislike funcs

--- server/services/user-service.js
+++ server/services/user-service.js
@@ -11,6 +11,30 @@ const UserModel = require('../models/User')
     async findByResetToken(token) {
         return this.model.find({ resetToken: token })
     }
+
+    async likeComment(user, comment) {
+        comment.like.push(user)
+        await comment.save()
+        return comment
+    }
+
+    async deleteLike(comment, index) {
+        comment.like.splice(index, 1)
+        await comment.save()
+        return comment
+    }
+
+    async dislikeComment(user, comment) {
+        comment.dislike.push(user)
+        await comment.save()
+        return comment
+    }
+
+    async deleteDislike(comment, index) {
+        comment.dislike.splice(index, 1)
+        await comment.save()
+        return comment
+    }
 }
 
 module.exports = new UserService()
